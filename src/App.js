4606a88ed1eca1b566480e8202e615d97fe20fa0
mdrayhanhowlader <rhowlader5314@gmail.com>
2022-12-04 03:11:49 +0600

    axios practiced done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "./axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/posts")
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       setPosts(res.data);
+  //     })
+  //     .catch((error) => {
+  //       setError(error.message);
+  //     });
+  // }, []);
+
+  const getApiData = async () => {
+    try {
+      const res = await axios.get("/posts");
+      setPosts(res.data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  useEffect(() => {
+    getApiData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Axios</h1>
+      <h2>{error !== "" && error}</h2>
+      <div className="posts">
+        {posts.slice(0, 12).map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title.slice(0, 15)}</h2>
+            <p>{post.body.slice(0, 100)}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
